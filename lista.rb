2194b7de10ca7a424857f6b8a3b7141ff5258bfe
Jake Chien <jake_chien@outlook.com>
2023-03-17 20:06:28 -0300

    learning map, each, while, for

--- lista.rb
+++ lista.rb
@@ -61,8 +61,32 @@ novo_user = {
 }
 
 listaDeUsers << novo_user
-# listaDeUsers.insert(3, novo_user)
-# puts listaDeUsers[0..2]
-get_user_by_id = listaDeUsers.select{ |user| user[:id] == 1 }
 puts "#{listaDeUsers}"
 
+# <------ aleatório ----->
+
+# listaDeUsers.insert(3, novo_user)
+# puts listaDeUsers[0..2]
+# get_user_by_id = listaDeUsers.select{ |user| user[:id] == 1 }
+
+# <------ for ------>
+# for user in listaDeUsers do
+#   puts user[:name]
+# end
+
+# <------ while ------>
+# count = 0
+# while count < listaDeUsers.length do
+#   puts listaDeUsers[count][:name]
+#   count += 1
+# end
+
+# <----- each ------->
+# listaDeUsers.each do |user|
+#   puts user[:name]
+# end
+
+# <----- map ------->
+# nome_alterado = listaDeUsers.map do |user|
+#   user[:name]
+# end
